9d1594f691465519e09dd02853179e36e565ba2d
nmasraf <nmasraf@users.noreply.github.com>
2015-05-25 01:18:58 +0800

    formatting changes, comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment#2: R function to cache potentially time-consuming computations
+## Calculating the inverse of matrix is time-consuming, so we want to reduce the time to do the inverse
+## by caching the result so tha it can be used again.
+## function makeCacheMatrix takes an input matrix and returns a list which essentially gets/sets mean into the 
+## vector and retrieves them if they exist already. makeCacheMatrix has the following libraries that achieves the above:
+# set the value of the vector
+# get the value of the vector
+# set the value of the mean
+# get the value of the mean
 
-## Write a short comment describing this function
+## the list x will be used in cacheSolve ()
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -21,7 +28,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve performs inverse of the special matrix only if it has not been calculated by makeCacheMatrix. 
+## If the inverse is already calculated, then function gets from the cache
 
 cacheSolve <- function(x, ...) {
         # takes the matrix from makeCacheMatrix and does the inverse
@@ -29,7 +37,7 @@ cacheSolve <- function(x, ...) {
         
         # has the inverse been calculated already?
         if (!is.null(inverse)){ 
-                # get it from the cache and skips the computation. 
+                # inverse available. skip computation 
                 message("getting cached data")
                 return(inverse)
         }
@@ -38,9 +46,8 @@ cacheSolve <- function(x, ...) {
         mat.data = x$get()
         inverse = solve(mat.data, ...)
         
-        # sets the value of the inverse in the cache via the setinv function.
+        # setinverse sets the value of the inverse in the cache 
         x$setinverse(inverse)
         
         return(inverse)
-        ## Return a matrix that is the inverse of 'x'
 }
