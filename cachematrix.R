7f91062bd4aad4baaf999144535875f43f5be3d4
nmasraf <nmasraf@users.noreply.github.com>
2015-05-24 20:46:43 +0800

    Update cachematrix.R
    Added comments for the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment#2: R function to cache potentially time-consuming computations
+## function makeCacheMatrix takes an input matrix and returns a list which essentially gets/sets mean into the 
+## vector and retrieves them if they exist already. makeCacheMatrix has the following libraries that achieves the above:
+# set the value of the vector
+# get the value of the vector
+# set the value of the mean
+# get the value of the mean
 
-## Write a short comment describing this function
+## the list x will be used in cacheSolve ()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
 }
